27881ee2233853d00b5ca9afdc4cec72c3cf1994
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-02 16:09:25 +0300

    making the hash from5 to 10

--- backend/pkg/middleware/log.go
+++ backend/pkg/middleware/log.go
@@ -8,7 +8,7 @@ import (
 
 // handle two func of bcrypt
 func HashPassword(password string) string {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 5)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	if err != nil {
 		fmt.Printf("Erreur lors du cryptage du mots de passe : %v \n", err)
 		return ""
